8acba5689e5e58be3af4224b244dc8708548b32e
Ramsha <ramshamm306@gmail.com>
2026-09-25 13:18:43 +0500

    chore: remove duplicate Frontend folder and clean root structure

--- Frontend/src/components/Vehicles/VehicleSearch.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SearchInput from "../common/SearchInput";
-
-export default function VehicleSearch({ value, onChange }) {
-  return (
-    <SearchInput value={value} onChange={onChange} placeholder="Search plate, type or slot" />
-  );
-}
